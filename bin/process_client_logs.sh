844f9bbb4be18ecaaca70ddd456b5d6cc523dabb
ColtDahl <dahlcolt@gmail.com>
2019-09-10 11:31:13 -0500

    Add pipe from grep into sed for preperation to replace

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,7 +3,8 @@ echo $tmp_dir
 for f in ../log_files/*.tgz; do tar -xzf $f  -C $tmp_dir; done
 ls -L $tmp_dir/var/log/
 cd $tmp_dir/var/log/
-grep -r '/\ ([a-zA-Z]   /   /' * > outputfile.txt
+#grep -r '\bJul\b \b21\b' * > outputfile.txt
+grep  -rl "Failed password" * | xargs sed -E ""  's/([A-Za-z]+) ([0-9])/wwww/g' > outputfile.txt
 #vi secure-20110724
 vi outputfile.txt
 
