a584ec8713c1ccef35c8679ee8e8d079f0641ce8
Jacob Peterson <pet02873@rynite.morris.umn.edu>
2019-09-10 10:33:52 -0500

    Create output file with grep -Colt

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,5 +3,7 @@ echo $tmp_dir
 for f in ../log_files/*.tgz; do tar -xzf $f  -C $tmp_dir; done
 ls -L $tmp_dir/var/log/
 cd $tmp_dir/var/log/
-vi secure-20110724
+grep -r '/\ ([a-zA-Z]   /   /' * > outputfile.txt
+#vi secure-20110724
+vi outputfile.txt
 
