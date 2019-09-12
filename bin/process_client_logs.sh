108b8089cbd37c02af80f32efda6a16fcd4c978d
ColtDahl <dahlcolt@gmail.com>
2019-09-11 19:21:52 -0500

    Fix regex oversight

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,11 +2,7 @@ cd $1
 for f in *.tgz; do tar -xzf $f  -C $1; done
 touch failed_login_data.txt
 grep  -rh "Failed password" --exclude="*.txt" . > failed_login_data.txt
-sed -Ein 's/([A-Za-z]+)\s*([0-9]+)\s*([0-9]+):..:..\s*([0-9a-zA-Z]+)\s*sshd\[.*\]:\s*Failed\s*password\s*for\s*([0-9a-zA-Z._ ]+)\s*from\s*([0-9].+)\s*port\s*([0-9]+)\s*..../\1 \2 \3 \4 \6 /p' "failed_login_data.txt"
+sed -Ein 's/([A-Za-z]+)\s*([0-9]+)\s*([0-9]+):..:..\s*([0-9a-zA-Z]+)\s*sshd\[.*\]:\s*Failed\s*password\s*for\s*([0-9a-zA-Z._\ -]+)\s*from\s*([0-9].+)\s*port\s*([0-9]+)\s*..../\1 \2 \3 \5 \6 /p' "failed_login_data.txt"
 sed -Ein 's/invalid user/ /p' "failed_login_data.txt"
-vi failed_login_data.txt
-
-#vi secure-20110724
-
-
-
+#vi failed_login_data.txt
+ 
