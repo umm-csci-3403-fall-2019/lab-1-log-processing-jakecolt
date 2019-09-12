3bcf6db93d7907c609dd44353cd60e788bcd1f44
ColtDahl <dahlcolt@gmail.com>
2019-09-12 12:33:13 -0500

    Add create_hours_dist, which is similar to create_username_dist

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,11 @@
+start_dir=$(pwd)
+cd $1
+input_dir=$(pwd)
+find -name 'failed_login_data.txt' -exec cat {} \; > failed_login_datafull.txt
+pwd
+touch tempfile.html
+sed -En 's/([A-Za-z]+)\s*([0-9]+)\s*([0-9]+)\s*([A-Za-z0-9_-]+)\s*([0-9]+).([0-9]+).([0-9]+).([0-9]+)/ \3/p' "failed_login_datafull.txt" | sort | uniq -c | sed -En  's/([0-9]+)  ([0-9]+)/data.addRow([\x27\2\x27,\1\]);/p' > tempfile.html
+cd "$start_dir"/html_components
+$start_dir/bin/wrap_contents.sh $input_dir/tempfile.html hours_dist $input_dir/hours_dist.html
+rm "$input_dir"/tempfile.html
+rm "$input_dir"/failed_login_datafull.txt
