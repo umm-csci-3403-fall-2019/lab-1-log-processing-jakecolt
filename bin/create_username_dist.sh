0f9e75efae838be50e59997ec75798a5bb58ff73
ColtDahl <dahlcolt@gmail.com>
2019-09-12 12:15:23 -0500

    Add search for failed_login_data in input folder

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,6 +1,12 @@
+start_dir=$(pwd)
 cd $1
+input_dir=$(pwd)
+find -name 'failed_login_data.txt' -exec cat {} \; > failed_login_datafull.txt
+pwd
 touch tempfile.html
-sed -En 's/([A-Za-z]+)\s*([0-9]+)\s*([0-9]+)\s*([A-Za-z0-9_-]+)\s*([0-9]+).([0-9]+).([0-9]+).([0-9]+)/ \4/p' "failed_login_data.txt" | sort | uniq -c | sed -En  's/([0-9]+)  ([A-Za-z0-9_-]+)/data.addRow([\x27\2\x27,\x27\1\x27]);/p' > tempfile.html
-cd ../html_components
-../bin/wrap_contents.sh $1/tempfile.html username_dist $1/username_dist.html
-rm $1/tempfile.html
+sed -En 's/([A-Za-z]+)\s*([0-9]+)\s*([0-9]+)\s*([A-Za-z0-9_-]+)\s*([0-9]+).([0-9]+).([0-9]+).([0-9]+)/ \4/p' "failed_login_datafull.txt" | sort | uniq -c | sed -En  's/([0-9]+)  ([A-Za-z0-9_-]+)/data.addRow([\x27\2\x27,\1\]);/p' > tempfile.html
+cd "$start_dir"/html_components
+$start_dir/bin/wrap_contents.sh $input_dir/tempfile.html username_dist $input_dir/username_dist.html
+ls -R "$input_dir"
+#mv username_dist.html $input_dir
+rm "$input_dir"/tempfile.html
