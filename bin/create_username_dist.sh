a18ee759bcc7c568190397109329eda3c573ebbb
ColtDahl <dahlcolt@gmail.com>
2019-09-12 12:21:08 -0500

    Some cleanup of temporary files after use of create_username_dish.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -7,6 +7,5 @@ touch tempfile.html
 sed -En 's/([A-Za-z]+)\s*([0-9]+)\s*([0-9]+)\s*([A-Za-z0-9_-]+)\s*([0-9]+).([0-9]+).([0-9]+).([0-9]+)/ \4/p' "failed_login_datafull.txt" | sort | uniq -c | sed -En  's/([0-9]+)  ([A-Za-z0-9_-]+)/data.addRow([\x27\2\x27,\1\]);/p' > tempfile.html
 cd "$start_dir"/html_components
 $start_dir/bin/wrap_contents.sh $input_dir/tempfile.html username_dist $input_dir/username_dist.html
-ls -R "$input_dir"
-#mv username_dist.html $input_dir
 rm "$input_dir"/tempfile.html
+rm "$input_dir"/failed_login_datafull.txt
