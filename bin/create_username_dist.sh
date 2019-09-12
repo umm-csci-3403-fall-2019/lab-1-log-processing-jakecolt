30889a0c64dd9f55d580a2269ee5c465945e17b5
ColtDahl <dahlcolt@gmail.com>
2019-09-11 20:49:22 -0500

    Add automatic creation of row filled html file with name username_dist.html

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,2 +1,6 @@
 cd $1
-sed -En 's/([A-Za-z]+)\s*([0-9]+)\s*([0-9]+)\s*([A-Za-z0-9_-]+)\s*([0-9]+).([0-9]+).([0-9]+).([0-9]+)/ \4/p' "failed_login_data.txt" | sort | uniq -c | sed -E 's/([0-9]+)  ([A-Za-z0-9_-]+)/ \1 \2 /p'
+touch tempfile.html
+sed -En 's/([A-Za-z]+)\s*([0-9]+)\s*([0-9]+)\s*([A-Za-z0-9_-]+)\s*([0-9]+).([0-9]+).([0-9]+).([0-9]+)/ \4/p' "failed_login_data.txt" | sort | uniq -c | sed -En  's/([0-9]+)  ([A-Za-z0-9_-]+)/data.addRow([\x27\2\x27,\x27\1\x27]);/p' > tempfile.html
+cd ../html_components
+../bin/wrap_contents.sh $1/tempfile.html username_dist $1/username_dist.html
+rm $1/tempfile.html
